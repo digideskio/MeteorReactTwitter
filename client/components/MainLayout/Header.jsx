f50532abf9928d025df5a991952ed4ff66ecc89e
Yuichi Hagio <yhagio87@gmail.com>
2015-12-09 07:55:15 -0500

    Inline style (begin)

--- client/components/MainLayout/Header.jsx
+++ client/components/MainLayout/Header.jsx
@@ -1,7 +1,12 @@
 Header = React.createClass({
   render() {
+    
+    let styles = {
+      backgroundColor: '#ddd'
+    };
+
     return (
-      <div className="header-box">
+      <div className="header-box" style={styles}>
         <AccountsUIWrapper />
         <hr />
       </div>
