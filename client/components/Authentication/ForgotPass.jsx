42acda068a5fb8bfae3cd34b2385720bbc409455
Yuichi Hagio <yhagio87@gmail.com>
2015-12-14 20:01:39 -0500

    Added styling for forgot password

--- client/components/Authentication/ForgotPass.jsx
+++ client/components/Authentication/ForgotPass.jsx
@@ -19,12 +19,50 @@ ForgotPass = React.createClass({
   },
 
   render() {
+    const styles = {
+      'form': {
+        'maxWidth': '700px',
+        'margin': '10px auto 20px'
+      },
+
+      'input': {
+        'display': 'block',
+        'width': 'calc(100% - 10px)',
+        'height': '40px',
+        'margin': '3px 0 20px 0',
+        'paddingLeft': '10px',
+        'backgroundColor': '#efefef',
+        'color': '#777',
+        'fontSize': '1.1em'
+      },
+
+      'btn': {
+        'height': '40px',
+        'width': '150px',
+        'backgroundColor': 'transparent',
+        'border': '2px solid #99da49',
+        'fontSize': '1.1em'
+      }
+    };
+
     return (
       <div className="">
-        <h1>Send New Password</h1>
-        <form onSubmit={this.handleSubmit}>
-          <p>Email: <input type="email" name="email" ref="emailInput" /></p>
-          <p><input type="submit" value="Send New Pass" /></p>
+        <h1>Reset Password</h1>
+        <p>*This will send reset password instruction to your email</p>
+        <form style={styles.form} onSubmit={this.handleSubmit}>
+
+          <label htmlFor="loginEmail">Email</label><br />
+          <span id="errEmail" className="error"></span>
+          <input
+            style={styles.input}
+            type="email"
+            ref="emailInput"
+            name="loginEmail"
+            placeholder="Your Email"
+            autoFocus
+            required />
+
+          <button type="submit" style={styles.btn}>Reset Password</button>
         </form>
       </div>
     );
